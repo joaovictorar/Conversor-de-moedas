479fbd6e05d6cd52c3c71e56d1cb75e20d810d28
joaovictorar <joao.victor.alvesr@gmail.com>
2023-10-25 23:12:27 -0300

    Alteração,html,css,js

---
+++ script.js
@@ -0,0 +1,24 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")//valor em Real
+    const currencyValueConverted = document.querySelector(".currency-value")// outras moedas
+    const dolarToday = 5
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US",{
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+
+    console.log(convertedValue)
+}
+
+convertButton.addEventListener("click", convertValues)
